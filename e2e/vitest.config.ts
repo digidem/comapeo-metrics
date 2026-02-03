af2040ba814d12d07e7ea216cf9671e8cd0baff3
Gregor MacLennan <gmaclennan@awana.digital>
2026-02-03 17:00:00 +0000

    other stuff

--- e2e/vitest.config.ts
+++ e2e/vitest.config.ts
@@ -2,10 +2,14 @@ import { defineWorkersConfig } from "@cloudflare/vitest-pool-workers/config";
 
 export default defineWorkersConfig({
   test: {
-    globalSetup: ["./global-setup.ts"],
     include: ["./**/*.test.ts"],
     testTimeout: 30_000,
     hookTimeout: 60_000,
+    coverage: {
+      provider: "istanbul",
+      reporter: ["text", "lcov"],
+      all: true,
+    },
     poolOptions: {
       workers: {
         wrangler: { configPath: "../worker/wrangler.jsonc" },
